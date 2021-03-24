5ad4ba1dde7eed6ae7288b5ca7a8bbbcb987047d
K S <roopashri.ks@thomsonreuters.com>
2021-03-24 22:32:34 +0530

    test

--- data.js
+++ data.js
@@ -1,90 +1,122 @@
 (function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
 
-      var myConnector = tableau.makeConnector();
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "content",
+            alias: "content",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "id",
+            alias: "id",
+            dataType: tableau.dataTypeEnum.int
+        },{
+            id: "requestType",
+            alias: "requestType",
+            dataType: tableau.dataTypeEnum.string
+        } ,{
+            id: "clientName",
+            alias: "clientName",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "legalName",
+            alias: "legalName",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "status",
+            alias: "status",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "requestNote",
+            alias: "requestNote",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "safeId",
+            alias: "safeId",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "dateCreated",
+            alias: "dateCreated",
+            dataType: tableau.dataTypeEnum.date
+        },
+        {
+            id: "dateQueued",
+            alias: "dateQueued",
+            dataType: tableau.dataTypeEnum.date
+        },
+        {
+            id: "registeredCountry",
+            alias: "registeredCountry",
+            dataType: tableau.dataTypeEnum.string
+        }
+        ];
 
-      // Define the schema
-      //  myConnector.getSchema = function(schemaCallback){}
+        var tableSchema = {
+            id: "Main",
+            alias: "Main",
+            columns: cols
+        };
 
-        myConnector.getSchema = function(schemaCallback) {
-          var recordsInfo = [];
-          $.ajax({
-            url: JSON.parse(tableau.connectionData)['url'],
-            type: "POST",
-            data: {
-              token: JSON.parse(tableau.connectionData)['token'],
-              content: 'exportFieldNames',
-              format: 'json',
-              returnFormat: 'json',
-              type: 'flat',
-              rawOrLabelHeaders: 'raw',
-              exportCheckboxLabel: 'true',
-              exportSurveyFields: 'true',
-              exportDataAccessGroups: 'true'
-              },
-            contentType: "application/x-www-form-urlencoded",
-            dataType: "json",
-            success: function(resp){
-                recordsInfo = resp;
-                var recordSchema = [];
-                recordsInfo.forEach(function(field){
-                  recordSchema.push({
-                    id: field.export_field_name,
-                    alias: field.original_field_name,
-                    dataType: tableau.dataTypeEnum.string
-                  });
-                });
-                var redcapTable = {
-                  id: "redcap",
-                  alias: "custom redcap extract",
-                  columns: recordSchema
-                }
-                schemaCallback([redcapTable]);
-              }
-            });
-         };
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+
+     /*   $.ajaxSetup({
+         headers : {
+            'x-rapidapi-host' : 'api-nba-v1.p.rapidapi.com',
+            'x-rapidapi-key' : 'd18cdeb3a2mshab6824adf713c9ep130fb2jsncca2d444588d'
+          } 
+        }); */
+
+        $.getJSON("http://pp.a206032-oem-backend.edp-content-preprod.aws-int.thomsonreuters.com/api/ai/rest/v1/search/requests?pageSize=1000000", function(resp) {
+            var feat = resp.raw_data,//countryVal = "India",
+                tableData = [];
+            
+            // Iterate over the JSON object
+             for (var j = 0, len = feat.length; j < len; j++) { 
+                
+
+               
+                    tableData.push({
+                    
+                        "content" : feat[j].content,
+                        "id": feat[j].id,
+                        "requestType":feat[j].requestType,
+                        "clientName":feat[j].clientName,
+                        "legalName": feat[j].legalName,
+                        "status": feat[j].status,
+                        "requestNote":feat[j].requestNote,
+                        "safeId": safeId,
+                        "dateCreated": feat[j].dateCreated,
+                        "dateQueued": feat[j].dateQueued,
+                        "registeredCountry":feat[j].registeredCountry
+                        
+                    });        
 
 
-      // Download the data
-      myConnector.getData = function(table, doneCallback) {
+                       
 
-        var tableData = [];
-          $.ajax({
-            url: JSON.parse(tableau.connectionData)['url'],
-            type: "POST",
-            data: {
-              token: JSON.parse(tableau.connectionData)['token'],
-              content: 'record',
-              format: 'json',
-              returnFormat: 'json',
-              type: 'flat',
-              rawOrLabelHeaders: 'raw',
-              exportCheckboxLabel: 'true',
-              exportSurveyFields: 'true',
-              exportDataAccessGroups: 'true'
-            },
-            contentType: "application/x-www-form-urlencoded",
-            dataType: "json",
-            success: function(resp){
-            resp.forEach(function(record){
-              tableData.push(record);
-            });
+                
+            } 
+            
+                    
+            
             table.appendRows(tableData);
             doneCallback();
-          }
         });
-        }
+    };
 
-      tableau.registerConnector(myConnector);
+    tableau.registerConnector(myConnector);
 
-      $(document).ready(function (){
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionData = JSON.stringify({
-              'token': $("#token").val(),
-              'url': $("#url").val()
-            });
-            tableau.connectionName = "Get Data";
-            tableau.submit();
+            tableau.connectionName = "Dataset"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
         });
-      });
-
-  })();
+    });
+})();
