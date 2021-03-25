16c5bae775fa554de061664a213b56b7734bf0eb
K S <roopashri.ks@thomsonreuters.com>
2021-03-25 08:36:20 +0530

    test

--- data.js
+++ data.js
@@ -5,6 +5,10 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
+            id: "content",
+            alias: "content",
+            dataType: tableau.dataTypeEnum.string
+        }/* ,{
             id: "id",
             alias: "id",
             dataType: tableau.dataTypeEnum.int
@@ -46,7 +50,7 @@
             id: "registeredCountry",
             alias: "registeredCountry",
             dataType: tableau.dataTypeEnum.string
-        }
+        } */
         ];
 
         var tableSchema = {
@@ -59,41 +63,28 @@
     };
 
     // Download the data
-/* var listdata =[]; */
-
-    
-/* 	function Callback(data){
-	for (i=0; i<data["items"].length; i++){
-		var list_name = data["items"][i]["name"];
-		var list_id = data["items"][i]["id"];
-		listData.push({"list": list_name});
-		};
-		 */	
-		
-	
-	// store list data, in tableau's connection data object
-	/* tableau.connectionData = JSON.stringify([listData])
-	tableau.connectionName = "Connector";
-	// this kicks off the getData() stage
-	tableau.submit(); */	
     myConnector.getData = function(table, doneCallback) {
-		var   tableData = [];
-		
-}
 
-        $.getJSON("http://pp.a206032-oem-backend.edp-content-preprod.aws-int.thomsonreuters.com/api/ai/rest/v1/search/requests?pageSize=1000000", function() {
-            var feat = resp.raw_data,//countryVal = "India",
-              
+     /*   $.ajaxSetup({
+         headers : {
+            'x-rapidapi-host' : 'api-nba-v1.p.rapidapi.com',
+            'x-rapidapi-key' : 'd18cdeb3a2mshab6824adf713c9ep130fb2jsncca2d444588d'
+          } 
+        }); */
+
+        $.getJSON("http://pp.a206032-oem-backend.edp-content-preprod.aws-int.thomsonreuters.com/api/ai/rest/v1/search/requests?pageSize=1000000", function(resp) {
+            var feat = resp.raw_data;//countryVal = "India",
+            var  tableData = [];
             
             // Iterate over the JSON object
-             for (var j = 0, len = 1000; j < len; j++) { 
+             for (var j = 0, len = feat.length; j < len; j++) { 
                 
 
                
                     tableData.push({
                     
-                        "firstName" : feat[j].firstName,
-                        "id": feat[j].id,
+                        "content" : feat[j].content,
+                       /*  "id": feat[j].id,
                         "requestType":feat[j].requestType,
                         "clientName":feat[j].clientName,
                         "legalName": feat[j].legalName,
@@ -102,9 +93,13 @@
                         "safeId": safeId,
                         "dateCreated": feat[j].dateCreated,
                         "dateQueued": feat[j].dateQueued,
-                        "registeredCountry":feat[j].registeredCountry
+                        "registeredCountry":feat[j].registeredCountry */
                         
-                    });                             
+                    });        
+
+
+                       
+
                 
             } 
             
@@ -120,12 +115,8 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-			
-	
-			
-		
             tableau.connectionName = "Dataset"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
-});
+})();
